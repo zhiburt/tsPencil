11586613b488293468477d61880789a18f3c7ce9
Maxim Zhiburt <zhiburt@gmail.com>
2018-01-29 22:46:25 +0300

    Index Css+Html

---
+++ src/init.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
