78e30dd47d8138ecd4a5ff50d3199121b9b8cca7
Maxim Zhiburt <zhiburt@gmail.com>
2018-01-31 20:08:27 +0300

    shit two

---
+++ dist/js/init.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        indicators: true
+    });
+  });
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
